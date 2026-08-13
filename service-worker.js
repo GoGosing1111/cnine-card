8213dae4c01425b236bedad7f5d26053d90c2c1c
Kimjihan <keyman1335@gmail.com>
2026-08-13 23:00:43 +0900

    Prevent stale evolution UI cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='soop-card-static-v1684';
+const CACHE_NAME='soop-card-static-v1685';
 const OFFLINE_URL='/offline.html';
 const CORE=[OFFLINE_URL,'/manifest.webmanifest','/assets/ui/pwa-icon.svg','/assets/ui/pwa-icon-maskable.svg'];
 
